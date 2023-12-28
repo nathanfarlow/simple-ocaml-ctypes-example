2e856f0ba35469a042790beea91a83f63e8c19ff
Nathan Farlow <natethegreatest11@gmail.com>
2023-12-27 20:16:17 -0500

    Add more comments

--- foo/foo.ml
+++ foo/foo.ml
@@ -1,7 +1,9 @@
 open! Ctypes
 
 (* After some processing, your types and functions modules will be available
-   through the C module. Now you can wrap them up in a nice OCaml wrapper. *)
+   through the C module. Now you can wrap them up in a nice OCaml wrapper. None
+   of these module names (Type_description, Function_description, C, C.Types, or
+   C.Functions) are special; you can change then in the dune file. *)
 
 let make_person ~name ~age =
   let person = make C.Types.Person.person in
